92d4eb0a9a2a2bd0c565799e6dd3d665016b336c
marryjianjian <logjian@gmail.com>
2021-11-09 00:30:52 +0800

    feat: get data from external API

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  serverRuntimeConfig: {
+    backend_url: process.env.BACKEND_URL,
+  },
   reactStrictMode: true,
+  poweredByHeader: false,
+  typescript: {
+    // !! WARN !!
+    // ignore error for now
+    ignoreBuildErrors: true,
+  },
 }
