b2fe2ccd002a7cc20113ce6c722a891c4487a02b
marryjianjian <logjian@gmail.com>
2021-12-05 17:06:53 +0800

    chore: adjust footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,14 +25,11 @@ const Home = (props: { last7dayscount: LastDaysInfo[], top10domainscount : HostI
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/marryjianjian/flows_front"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by{' '}flows_statistics
         </a>
       </footer>
     </div>
